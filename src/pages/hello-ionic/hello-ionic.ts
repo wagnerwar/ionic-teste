4fc30b8a916efae3d53f188bbda720cc96257e7a
Wagner <nuneswar2000@hotmail.com>
2017-09-25 14:40:04 -0300

    Alterações

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
-
+  constructor(public alertCtrl: AlertController) {
+     this.showAlert();
+  }
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Olá Amigo!',
+      subTitle: 'Seja bem vindo!',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 }
